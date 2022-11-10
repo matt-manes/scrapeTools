b24040e41f87284e020da28d4edf3e8e29195397
matt manes <113395539+matt-manes@users.noreply.github.com>
2022-11-09 19:34:36 -0600

    Delete src/scrapetools directory

--- src/scrapetools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .emailScraper import scrapeEmails
-from .linkScraper import LinkScraper
-from .phoneScraper import scrapePhoneNumbers
-
-__all__ = ['scrapeEmails','LinkScraper','scrapePhoneNumbers']
